9eedaa7b3473c4eac2109c667fcf653a05d81154
Luiz Pelizaro <luiz.pelizaro127@hotmail.com>
2023-10-14 13:15:59 -0300

    refac math_functions.py

--- src/math/math_functions.py
+++ src/math/math_functions.py
@@ -11,32 +11,36 @@ def pilot_time(risk, stamina):
         print(f'Insert valid value for risk high or medium : {e}')
 
 
-def lap_factor(pilot_time, lap_time):
+def calculate_lap_factor(pilot_time, lap_time):
     try:
-        return lap_time / pilot_time
+        lap_factor = lap_time / pilot_time
+        return lap_factor
     except ZeroDivisionError as e:
         print(f'Time invalid !!\n{e}')
 
 
 def piloted_laps(stamina, value_lap_factor):
     try:
-        return stamina / value_lap_factor
+        estimated_piloted_laps = stamina / value_lap_factor
+        return estimated_piloted_laps
     except ZeroDivisionError as e:
         print(f'Value invalid !!\n {e}')
 
 
 def pit_stop_estimation(number_of_turns_of_fuel_added):
     try:
-        return 30 + (0.235 * number_of_turns_of_fuel_added)
+        estimated_pit = 30 + (0.235 * number_of_turns_of_fuel_added)
+        return estimated_pit
     except TypeError as e:
-        print(f'Insert value valid of number of number_of_turns_of_fuel_added \n {e}')
+        print(f'Insert value valid of number of number_of_turns_of_fuel_added !! \n {e}')
 
 
 def num_lap_estimate(race_time, lap_time):
     try:
         time_in_seconds = race_time * 60
         estimate_lap_time = lap_time + 2
-        return time_in_seconds / estimate_lap_time
+        num_laps = time_in_seconds / estimate_lap_time
+        return num_laps
     except ValueError as e:
         print(f'Input value valid !! \n {e}')
     except ZeroDivisionError as e:
